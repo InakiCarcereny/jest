f953d3d65bc811e19fed6744931d7eb2769b7dd1
inakicarcereny <inakicarce@hotmail.com.ar>
2024-12-16 17:14:44 -0300

    Add Counter component input for setting count; enhance tests for initial value render and increment behavior

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -8,6 +8,12 @@ describe("Counter", () => {
     expect(counter).toBeInTheDocument();
   });
 
+  it("renders a count of 0", () => {
+    render(<Counter />);
+    const counter = screen.getByRole("paragraph");
+    expect(counter).toHaveTextContent("counter: 0");
+  });
+
   it("increments the counter", () => {
     render(<Counter />);
     const increment = screen.getByText("increment");
@@ -15,5 +21,23 @@ describe("Counter", () => {
 
     const counter = screen.getByText("counter: 1");
     expect(counter).toBeInTheDocument();
+  });
+
+  it("increments the counter", () => {
+    render(<Counter />);
+    const increment = screen.getByText("increment");
+    for (let i = 0; i < 2; i++) {
+      fireEvent.click(increment);
+    }
+
+    const counter = screen.getByText("counter: 2");
+    expect(counter).toBeInTheDocument();
+  });
+
+  it("sets the value of the counter", () => {
+    render(<Counter />);
+    const input = screen.getByRole("spinbutton");
+    fireEvent.change(input, { target: { value: 10 } });
+    expect(input).toHaveValue(10);
   });
 });
--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -2,11 +2,20 @@ import { useState } from "react";
 
 export function Counter() {
   const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(0);
 
   return (
     <div className="flex flex-col">
       <p>counter: {count}</p>
       <button onClick={() => setCount(count + 1)}>increment</button>
+      <input
+        type="number"
+        name="amount"
+        value={amount}
+        onChange={(e) => setAmount(parseInt(e.target.value))}
+      />
+
+      <button onClick={() => setCount(amount)}>Set</button>
     </div>
   );
 }
