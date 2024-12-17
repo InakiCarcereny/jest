30496dc3b5d357b1d4a55f23a9b56c49d07f3e35
inakicarcereny <inakicarce@hotmail.com.ar>
2024-12-17 13:20:56 -0300

    refactor layout to use ThemeProvider and update Counter tests to cover new focus behavior and button functionality

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -2,12 +2,6 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { Counter } from "../app/components/Counter";
 
 describe("Counter", () => {
-  it("renders the counter", () => {
-    render(<Counter />);
-    const counter = screen.getByText("counter: 0");
-    expect(counter).toBeInTheDocument();
-  });
-
   it("renders a count of 0", () => {
     render(<Counter />);
     const counter = screen.getByRole("paragraph");
@@ -39,5 +33,29 @@ describe("Counter", () => {
     const input = screen.getByRole("spinbutton");
     fireEvent.change(input, { target: { value: 10 } });
     expect(input).toHaveValue(10);
+
+    const button = screen.getByRole("button", {
+      name: "Set",
+    });
+
+    fireEvent.click(button);
+    const paragraph = screen.getByRole("paragraph");
+    expect(paragraph).toHaveTextContent("counter: 10");
+  });
+
+  it("elements ara focused in the right order", () => {
+    render(<Counter />);
+    const counter = screen.getByRole("paragraph");
+    const amountInput = screen.getByRole("spinbutton");
+    const incrementButton = screen.getByRole("button", {
+      name: "increment",
+    });
+    const setButton = screen.getByRole("button", {
+      name: "Set",
+    });
+    expect(counter).toHaveFocus();
+    expect(amountInput).toHaveFocus();
+    expect(incrementButton).toHaveFocus();
+    expect(setButton).toHaveFocus();
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { Contador } from "./components/Contador";
+import { Theme } from "./components/Theme";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center text-xl font-semibold">
-      hola mundo
-      <Contador />
-      <Contador />
-      <button>Click me</button>
-      <input type="search" placeholder="search..." />
+      <Theme />
     </div>
   );
 }
